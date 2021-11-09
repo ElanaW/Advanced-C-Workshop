6abeeca675c6fbe46f44c0bce2064a89396241f5
Elana <elweiss@g.jct.ac.il>
2021-11-09 09:53:44 +0200

    Added comments to functions in 3.2

--- Homework 3/Assignment 3.2/Array.h
+++ Homework 3/Assignment 3.2/Array.h
@@ -10,9 +10,14 @@ void swap(int* val1, int* val2);
 
 /**
  * @brief function sorts values in array from smallest to greatest
- * @param address of 2 values
+ * @param array, and size of array
  */
 void sort(int* array, int size);
-void printArray(int array[], int size);
+
+/**
+ * @brief function prints values in an array
+ * @param array, and size of array
+ */
+void printArray(int* array, int size);
 
 #endif //ARRAY_H_
